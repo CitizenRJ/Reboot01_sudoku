e94ea84c05f7001910e7f09a8e44480a26eaca2b
naalamri <nayef.m.alamri@gmail.com>
2023-05-26 22:34:26 +0300

    Sudoku

---
+++ sudoku.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func NoDuplicate(board [][]int, row, col, num int) bool {
+	// check if the number is already in the row
+	for i := 0; i < 9; i++ {
+		if board[row][i] == num && i != col {
+			return false
+		}
+	}
+	// check if the number is already in the column
+	for i := 0; i < 9; i++ {
+		if board[i][col] == num && row != i {
+			return false
+		}
+	}
+	// check if the number is already in the 3x3 grid // i for the box inside the row / j box in culomn
+	gridRow, gridCol := row/3*3, col/3*3
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			if board[gridRow+i][gridCol+j] == num && (gridRow+i != row && gridCol+j != col) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func main() {
+	if !ValidateArgs() {
+		fmt.Println("Error")
+		return
+	}
+	// to build the table
+	table := BuildTable()
+	// check duplicates
+	for x, row := range table {
+		for y, n := range row {
+			if table[x][y] == 0 {
+				continue
+			}
+			if !NoDuplicate(table, x, y, n) {
+				fmt.Println("Error")
+				return
+			}
+		}
+	}
+	solveSudoku(table)
+	printBoard(table)
+}
+
+func solveSudoku(board [][]int) bool {
+	row, col := 0, 0
+	foundEmptyCell := false
+	// find the next empty cell
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			if board[i][j] == 0 {
+				row, col = i, j
+				foundEmptyCell = true
+				break
+			}
+		}
+		if foundEmptyCell {
+			break
+		}
+	}
+	// if there are no more empty cells, the Sudoku is solved
+	if !foundEmptyCell {
+		return true
+	}
+	// try to fill in the empty cell with a valid number
+	for num := 1; num <= 9; num++ {
+		if isValid(board, row, col, num) {
+			board[row][col] = num
+			if solveSudoku(board) {
+				return true
+			}
+			board[row][col] = 0
+		}
+	}
+	// if no valid number was found, backtrack
+	return false
+}
+
+func isValid(board [][]int, row, col, num int) bool {
+	// check if the number is already in the row
+	for i := 0; i < 9; i++ {
+		if board[row][i] == num {
+			return false
+		}
+	}
+	// check if the number is already in the column
+	for i := 0; i < 9; i++ {
+		if board[i][col] == num {
+			return false
+		}
+	}
+	// check if the number is already in the 3x3 grid
+	gridRow, gridCol := row/3*3, col/3*3
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			if board[gridRow+i][gridCol+j] == num {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func printBoard(board [][]int) {
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			fmt.Print(board[i][j])
+			if j != 8 {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func BuildTable() [][]int {
+	args := os.Args[1:]
+	_table := make([][]int, 9)
+	for i := 0; i < 9; i++ {
+		_table[i] = make([]int, 9)
+	}
+	for x, row := range _table {
+		for y := range row {
+			if args[x][y] == '.' {
+				_table[x][y] = 0
+			} else {
+				_table[x][y] = int(args[x][y] - 48)
+			}
+		}
+	}
+	return _table
+}
